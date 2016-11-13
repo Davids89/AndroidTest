74e1e03f0539d7a356debb69de7ae4aa131d11dc
Davids89 <daviluqui@gmail.com>
2016-11-13 13:53:47 -0800

    Comics event for eventBus

--- app/src/main/java/com/redbooth/comics/ComicsList/ComicsEvent.java
+++ app/src/main/java/com/redbooth/comics/ComicsList/ComicsEvent.java
@@ -1,8 +1,21 @@
 package com.redbooth.comics.ComicsList;
 
+import com.redbooth.comics.entities.Marvel;
+
+import java.util.List;
+
 /**
  * Created by david on 13/11/16.
  */
 
 public class ComicsEvent {
+    List<Marvel.Data.Comic> comics;
+
+    public List<Marvel.Data.Comic> getComics() {
+        return comics;
+    }
+
+    public void setComics(List<Marvel.Data.Comic> comics) {
+        this.comics = comics;
+    }
 }
